b1ae6619d0423952e33486b3ed6ebf8929cc1acd
Aim4 <aimforking@gmail.com>
2020-10-08 16:51:34 -0400

    Adjust player speeds and jump, and rename tiles

--- src/Actors/Player.cs
+++ src/Actors/Player.cs
@@ -4,8 +4,8 @@ using System;
 public class Player : KinematicBody2D
 {
     const int GRAVITY = 20;
-    const int SPEED = 100;
-    const int JUMP_POWER = -250;
+    const int SPEED = 125;
+    const int JUMP_POWER = -350;
     public Vector2 UP = Vector2.Up;
     private Vector2 motion ;
 
