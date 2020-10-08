24e8ff3b4b3c290d55a183be36f77b0b3d6fbdec
Aim4 <aimforking@gmail.com>
2020-10-08 15:52:33 -0400

    Fix player jumping physics

---
+++ src/Actors/Player.cs
@@ -0,0 +1,41 @@
+using Godot;
+using System;
+
+public class Player : KinematicBody2D
+{
+    const int GRAVITY = 20;
+    const int SPEED = 100;
+    const int JUMP_POWER = -250;
+    public Vector2 UP = Vector2.Up;
+    private Vector2 motion ;
+
+    public override void _Ready()
+    {
+        
+    }
+
+    public override void _PhysicsProcess(float delta)
+    {
+        motion.x = 0;
+        motion.y += GRAVITY; // gravity
+        if (Input.IsActionPressed("ui_right"))
+        {
+            motion.x += SPEED;
+        }
+        else if (Input.IsActionPressed("ui_left"))
+        {
+            motion.x -= SPEED;
+        }
+
+        //if (this.IsOnFloor() && Input.IsActionPressed("ui_up"))
+        if (Input.IsActionPressed("ui_up"))
+        {
+            if (this.IsOnFloor())
+            {
+                motion.y += JUMP_POWER;
+            }
+        }
+
+        motion = this.MoveAndSlide(motion, UP);
+    }
+}
