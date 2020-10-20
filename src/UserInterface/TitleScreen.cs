e88655015dfab92f7be8608a9e9fc7b025e5189e
Aim4 <aimforking@gmail.com>
2020-10-20 14:26:41 -0400

    Add signals to buttons

---
+++ src/UserInterface/TitleScreen.cs
@@ -0,0 +1,35 @@
+using Godot;
+using System;
+
+public class TitleScreen : Control
+{
+
+    const string BUTTON_PARENT_PATH = "TitleContent/VBoxContainer/CenterContainer/VBoxContainer/";
+    private Button startBtn;
+    private Button settingsBtn;
+    private Button quitBtn;
+    public override void _Ready()
+    {
+        startBtn = GetNode<Button>(BUTTON_PARENT_PATH + "Start");
+        settingsBtn = GetNode<Button>(BUTTON_PARENT_PATH + "Settings");
+        quitBtn = GetNode<Button>(BUTTON_PARENT_PATH + "Quit");
+    }
+    public override void _PhysicsProcess(float delta)
+    {
+        //
+    }
+
+    public void OnStartPressed()
+    {
+        // Load Game
+    }
+
+    public void OnSettingsPressed()
+    {
+        // Load settings
+    }
+    public void OnQuitPressed()
+    {
+        GetTree.Quit();
+    }
+}
