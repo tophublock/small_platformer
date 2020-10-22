def8991f07a713b1d849304efc9934bdafc8e9ac
Aim4 <aimforking@gmail.com>
2020-10-22 13:54:18 -0400

    Change scene when player enters level transition door

---
+++ src/Objects/LevelTransition.cs
@@ -0,0 +1,23 @@
+using Godot;
+using System;
+
+public class LevelTransition : Area2D
+{
+
+    [Export(PropertyHint.File, "*.tscn")]
+    public string TargetStage;
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        
+    }
+
+    public void OnLevelTransitionBodyEntered(Node body)
+    {
+        if (body is Player)
+        {
+            GetTree().ChangeScene(TargetStage);
+        }
+    }
+}
