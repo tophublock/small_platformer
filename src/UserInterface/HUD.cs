2ebbfef5be401a94173fe41dd2f463c8374ed49a
Aim4 <aimforking@gmail.com>
2020-10-22 14:54:34 -0400

    add HUD

---
+++ src/UserInterface/HUD.cs
@@ -0,0 +1,16 @@
+using Godot;
+using System;
+
+public class HUD : CanvasLayer
+{
+    private Label _livesLabel;
+    public override void _Ready()
+    {
+        _livesLabel = GetNode<Label>("Panel/CenterContainer/MarginContainer/HBoxContainer/HBoxContainer/Lives");
+    }
+
+    public void UpdateLives(int lives)
+    {
+        _livesLabel.Text = lives.ToString();
+    }
+}
