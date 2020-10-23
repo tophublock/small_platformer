9640882d534ec55dd19e3dd15244185abb8c4825
Aim4 <aimforking@gmail.com>
2020-10-23 14:09:02 -0400

    Reload scene if playing again

--- src/Main/Level0.cs
+++ src/Main/Level0.cs
@@ -3,13 +3,14 @@ using System;
 
 public class Level0 : Node
 {
-
+    public string levelName;
     private HUD _hud;
     private Player _player;
     private PackedScene _gameOverScreen;
 
     public override void _Ready()
     {
+        levelName = this.Name;
         _hud = GetNode<HUD>("HUD");
         _player = GetNode<Player>("Player");
         _gameOverScreen = ResourceLoader.Load("res://src/UserInterface/GameOverScreen.tscn") as PackedScene;
@@ -26,5 +27,12 @@ public class Level0 : Node
     {
         var gameOver = _gameOverScreen.Instance() as GameOverScreen;
         AddChild(gameOver);
+
+        gameOver.Connect("PlayAgain", this, nameof(Restart));
+    }
+
+    private void Restart()
+    {
+        GetTree().ChangeScene($"res://src/Main/{levelName}.tscn");
     }
 }
