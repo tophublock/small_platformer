b4472b845708169caec0e1025e4bcbb9fb8337e9
Aim4 <aimforking@gmail.com>
2020-10-23 14:00:06 -0400

    Add play again signal to game over screen

---
+++ src/UserInterface/GameOverScreen.cs
@@ -0,0 +1,29 @@
+using Godot;
+using System;
+
+public class GameOverScreen : CanvasLayer
+{
+    [Signal]
+    public delegate void PlayAgain();
+
+    const string BTN_PATH = "PanelContainer/MarginContainer/CenterContainer/VBoxContainer/MarginContainer/VBoxContainer/";
+
+    private Button _playAgainBtn;
+    private Button _quitBtn;
+
+    public override void _Ready()
+    {
+        _playAgainBtn = GetNode<Button>(BTN_PATH + "PlayAgain");
+        _quitBtn = GetNode<Button>(BTN_PATH + "Quit");
+    }
+
+    public void OnPlayAgainPressed()
+    {
+        EmitSignal(nameof(PlayAgain));
+    }
+
+    public void OnQuitPressed()
+    {
+        GetTree().Quit();
+    }
+}
